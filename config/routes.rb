fa17ae168b1d33e2a1768d6e9629369cc0a292b7
nagatadaichi <gg.daichi@gmail.com>
2019-08-21 17:11:40 +0900

    画面実装とDB更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-  root to:'messages#index'
+  get 'home/index'
+  root to:'home#index'
+
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
