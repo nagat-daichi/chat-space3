6f3e7ac968df964d99364570e1286bd1eaf8e024
nagatadaichi <gg.daichi@gmail.com>
2019-08-28 11:54:54 +0900

    DBと実装画面２
    aap/view/home
    READMEの更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-  root to:'messages#index'
+  get 'home/index'
+  root to:'home#index'
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
