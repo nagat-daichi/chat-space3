e9a3df7962c439bb03e65241b72ab4b8ced5a16d
nagatadaichi <gg.daichi@gmail.com>
2019-08-29 12:11:28 +0900

    修正しますた
    すみませんでした。特に理由はなかったです。

--- db/migrate/20190828084601_create_messages.rb
+++ db/migrate/20190828084601_create_messages.rb
@@ -1,11 +1,11 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.neme
-      t.messages
-      t.timestamps
-      t.image
-      t.text
+      t.string :neme
+      t.string :messages
+      t.string :timestamps
+      t.string :image
+      t.string :text
     end
   end
 end
